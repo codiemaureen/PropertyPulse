f92721cdec74183cc25d03b9f9c1e128c65622e7
Codiemaureen <codiestover.cs@gmail.com>
2025-04-30 14:25:24 -0400

    Adding useSearchParams

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,13 +1,15 @@
 'use client';
-import { useRouter, useParams } from "next/navigation";
+import { useRouter, useParams, useSearchParams } from "next/navigation";
 
 
 const PropertyPage = () => {
  const router = useRouter();
  const params = useParams();
+ const searchParams = useSearchParams();
 
  return ( <div>
   <h1>Property Page {params.id}</h1>
+  <p>{searchParams.get('name')}</p>
   <button onClick={() => router.replace('/')}>Go Home</button>
  </div> );
 }
