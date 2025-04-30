19e723876b12ad01b6966a13049dde32e22256fe
Codiemaureen <codiestover.cs@gmail.com>
2025-04-30 14:32:14 -0400

    Removing unnecessary code

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,18 +1,7 @@
-'use client';
-import { useRouter, useParams, useSearchParams, usePathname } from "next/navigation";
-
-
-const PropertyPage = () => {
- const router = useRouter();
- const params = useParams();
- const searchParams = useSearchParams();
- const pathname = usePathname();
+const PropertyPage = ({ params }) => {
 
  return ( <div>
-  <h1>Property Page {params.id}</h1>
-  <p>{searchParams.get('name')}</p>
-  <p>{pathname}</p>
-  <button onClick={() => router.replace('/')}>Go Home</button>
+  Property page {params.id}
  </div> );
 }
  
