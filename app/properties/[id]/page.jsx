f227a921ba3b698b7d779bd6e0f44b242b4abea7
Codiemaureen <codiestover.cs@gmail.com>
2025-04-30 14:30:53 -0400

    Adding use pathname

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,15 +1,17 @@
 'use client';
-import { useRouter, useParams, useSearchParams } from "next/navigation";
+import { useRouter, useParams, useSearchParams, usePathname } from "next/navigation";
 
 
 const PropertyPage = () => {
  const router = useRouter();
  const params = useParams();
  const searchParams = useSearchParams();
+ const pathname = usePathname();
 
  return ( <div>
   <h1>Property Page {params.id}</h1>
   <p>{searchParams.get('name')}</p>
+  <p>{pathname}</p>
   <button onClick={() => router.replace('/')}>Go Home</button>
  </div> );
 }
