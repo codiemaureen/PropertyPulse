84b9d8f12176944879453128673477286d2637c3
Codiemaureen <codiestover.cs@gmail.com>
2025-05-02 14:22:33 -0400

    Fix: code clean up

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,8 +3,10 @@ import Image from "next/image";
 import logo from '@/assets/images/logo.png'
 
 const Footer = () => {
- const currentYear = new Date().getFullYear();
- return (     <footer className="bg-gray-200 py-4 mt-auto">
+  const currentYear = new Date().getFullYear();
+  return (     
+
+  <footer className="bg-gray-200 py-4 mt-auto">
       <div
         className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4"
       >
@@ -14,10 +16,10 @@ const Footer = () => {
         <div
           className="flex flex-wrap justify-center md:justify-start mb-4 md:mb-0"
         >
-          <ul className="flex space-x-4">
-            <li><Link href="/properties.html">Properties</Link></li>
-            <li><Link href="/terms.html">Terms of Service</Link></li>
-          </ul>
+        <ul className="flex space-x-4">
+          <li><Link href="/properties.html">Properties</Link></li>
+          <li><Link href="/terms.html">Terms of Service</Link></li>
+        </ul>
         </div>
         <div>
           <p className="text-sm text-gray-500 mt-2 md:mt-0">
@@ -25,7 +27,8 @@ const Footer = () => {
           </p>
         </div>
       </div>
-    </footer> );
+  </footer> 
+  );
 }
- 
+
 export default Footer;
