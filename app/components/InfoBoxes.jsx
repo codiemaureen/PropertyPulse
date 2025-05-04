5eb138afde382a7c637f319ca7ea642caf12144e
Codiemaureen <codiestover.cs@gmail.com>
2025-05-04 14:21:08 -0400

    Fix: Importing infoBox to infoBoxes

--- app/components/InfoBoxes.jsx
+++ app/components/InfoBoxes.jsx
@@ -1,21 +1,11 @@
+import InfoBox from "./InfoBox";
+
 const InfoBoxes = () => {
  return ( 
       <section>
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-          <div className="bg-gray-100 p-6 rounded-lg shadow-md">
-            <h2 className="text-2xl font-bold">For Renters</h2>
-            <p className="mt-2 mb-4">
-              Find your dream rental property. Bookmark properties and contact
-              owners.
-            </p>
-            <a
-              href="/properties.html"
-              className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
-            >
-              Browse Properties
-            </a>
-          </div>
+          <InfoBox />
           <div className="bg-blue-100 p-6 rounded-lg shadow-md">
             <h2 className="text-2xl font-bold">For Property Owners</h2>
             <p className="mt-2 mb-4">
