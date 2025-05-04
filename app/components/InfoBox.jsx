c26a047af856cb7fefd0329fbb8bc09f05e1a006
Codiemaureen <codiestover.cs@gmail.com>
2025-05-04 14:46:27 -0400

    Adding props to infobox

--- app/components/InfoBox.jsx
+++ app/components/InfoBox.jsx
@@ -1,16 +1,15 @@
-const InfoBox = () => {
+const InfoBox = ({ heading, backgroundColor='bg-gray-100', textColor='text-gray-800', buttonInfo, children }) => {
  return ( <div>
-            <div className="bg-gray-100 p-6 rounded-lg shadow-md">
-            <h2 className="text-2xl font-bold">For Renters</h2>
-            <p className="mt-2 mb-4">
-              Find your dream rental property. Bookmark properties and contact
-              owners.
+            <div className={`${backgroundColor} p-6 rounded-lg shadow-md`}>
+            <h2 className={`${textColor} text-2xl font-bold`}>{ heading }</h2>
+            <p className={`${textColor} mt-2 mb-4`}>
+              { children }
             </p>
             <a
-              href="/properties.html"
-              className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
+              href={buttonInfo.link}
+              className={`${buttonInfo.backgroundColor} inline-block text-white rounded-lg px-4 py-2 hover:bg-gray-700`}
             >
-              Browse Properties
+              {buttonInfo.text}
             </a>
           </div>
  </div> );
