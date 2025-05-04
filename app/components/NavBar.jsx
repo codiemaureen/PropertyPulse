75d41c9fbc00282e320ca57ff6764340b34ffedd
Codiemaureen <codiestover.cs@gmail.com>
2025-05-04 19:07:04 -0400

    Fix: fixing home link

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -52,7 +52,7 @@ const NavBar = () => {
               className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
             >
               {/* <!-- Logo --> */}
-              <Link className="flex flex-shrink-0 items-center" href="/index.html">
+              <Link className="flex flex-shrink-0 items-center" href="/">
                 <Image
                   className="h-10 w-auto"
                   src={logo}
