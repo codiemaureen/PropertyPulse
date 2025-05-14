5dfacf5ba91378cef3841e785d6985bf60425bb7
Codiemaureen <codiestover.cs@gmail.com>
2025-05-14 11:48:39 -0400

    Fix: code clean up

--- app/page.jsx
+++ app/page.jsx
@@ -4,11 +4,13 @@ import InfoBoxes from "./components/InfoBoxes";
 import HomeProperties from "./components/HomeProperties";
 
 const HomePage = () => {
- return ( <div>
-  <Hero />
-  <InfoBoxes />
-  <HomeProperties />
-</div> );
+  return ( 
+    <div>
+      <Hero />
+      <InfoBoxes />
+      <HomeProperties />
+    </div> 
+  );
 }
 
 export default HomePage;
