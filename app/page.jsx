8ea9a2257e6a845b6fdf7010010c230aa0b66e51
Codiemaureen <codiestover.cs@gmail.com>
2025-05-15 17:20:49 -0400

    Fix: code clean up

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Hero from "./components/Hero";
 import InfoBoxes from "./components/InfoBoxes";
 import HomeProperties from "./components/HomeProperties";
-import connectDB from "@/config/database";
+
 
 const HomePage = () => {
   connectDB();
