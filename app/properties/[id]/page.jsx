c32a10b1e49f1f22f07298a6826278610f308737
Codiemaureen <codiestover.cs@gmail.com>
2025-05-19 09:25:00 -0400

    Add: Adding property info section

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,6 +1,8 @@
 import PropertyHeaderImage from "@/app/components/PropertyHeaderImage";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
 
 const PropertyPage = async ({ params }) => {
   await connectDB();
@@ -9,7 +11,23 @@ const PropertyPage = async ({ params }) => {
  return ( 
    <>
     <PropertyHeaderImage image={property.images[0]} />
-    <section>{property.name}</section>
+    <section>
+      <div className="container m-auto py-6 px-6">
+        <Link
+          href="/properties"
+          className="text-blue-500 hover:text-blue-600 flex items-center"
+        >
+          <FaArrowLeft className="mr-2"/> Back to Properties
+        </Link>
+      </div>
+    </section>
+    <section className="bg-blue-50">
+      <div className="container m-auto py-10 px-6">
+        <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+          {/* Property Info */}
+        </div>    
+      </div>    
+    </section>
    </>
  );
 }
