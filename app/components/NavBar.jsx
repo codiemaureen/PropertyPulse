216c36ed69d338a49f35bb8f1d310bbe14d3a385
Codiemaureen <codiestover.cs@gmail.com>
2025-05-23 09:52:00 -0400

    Closing dropdown menu onClick

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -190,6 +190,7 @@ const NavBar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-0"
+                      onClick={() => setIsProfileMenuOpen(false)}
                       >Your Profile</Link
                     >
                     <Link
@@ -198,6 +199,7 @@ const NavBar = () => {
                       role="menuitem"
                       tabIndex="-1"
                       id="user-menu-item-2"
+                      onClick={() => setIsProfileMenuOpen(false)}
                       >Saved Properties</Link
                     >
                     <button
