c70dd598ce2acf6920d65cb8116f95fc4e7011f6
Codiemaureen <codiestover.cs@gmail.com>
2025-05-22 20:53:20 -0400

    Fixing to property link

--- app/components/ProfileProperties.jsx
+++ app/components/ProfileProperties.jsx
@@ -7,7 +7,7 @@ const ProfileProperties = ({properties: initialProperties}) => {
  const [properties, setProperties] = useState(initialProperties);
  return properties.map((property) => (
             <div key={property._id}className="mb-10">
-              <Link href="/property">
+              <Link href={`/properties/${property._id}`}>
                 <Image
                   className="h-32 w-full rounded-md object-cover"
                   src={property.images[0]}
