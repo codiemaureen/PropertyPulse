796572a6ee421dc8cfdf4bc6c1304c71012c7d6d
Codiemaureen <codiestover.cs@gmail.com>
2025-05-27 15:26:23 -0400

    Getting property from params

--- app/properties/[id]/edit/page.jsx
+++ app/properties/[id]/edit/page.jsx
@@ -1,6 +1,20 @@
-const { default: PropertyEditForm } = require("@/app/components/PropertyEditForm")
+import PropertyEditForm from "@/app/components/PropertyEditForm";
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { convertToSerializableObject } from "@/utils/convertToObject";
 
-const PropertyEditPage = () => {
+const PropertyEditPage = async ({ params }) => {
+ await connectDB();
+
+ const propertyDoc = await Property.findById(params.id).lean();
+ const property = convertToSerializableObject(propertyDoc);
+
+ if(!property){
+  return <h1 className="text-center text-2xl font-bold mt-10">
+   Property not found
+  </h1>
+ }
+
  return ( 
   <section className="bg-blue-50">
    <div className="container m-auto max-w-2xl py-24">
