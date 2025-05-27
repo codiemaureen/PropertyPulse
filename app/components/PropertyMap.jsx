e5980364e1cc7f2b7a62f32f31fd5ec01722727e
Codiemaureen <codiestover.cs@gmail.com>
2025-05-27 19:22:33 -0400

    Importing map packages

--- app/components/PropertyMap.jsx
+++ app/components/PropertyMap.jsx
@@ -1,4 +1,11 @@
+'use client';
+import { useEffect, useState } from "react";
+import { setDefaults, fromAddress } from "react-geocode";
+
+
 const PropertyMap = ({property}) => {
+ const [lat, setLat] =  useState(null);
+ const [long, setLong] = useState(null);
  return ( 
   <div>map</div>
   );
