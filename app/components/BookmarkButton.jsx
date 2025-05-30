34a509cfcd92d22e2a9d3a5a1bca123b7a960d95
Codiemaureen <codiestover.cs@gmail.com>
2025-05-29 20:05:55 -0400

    Adding checkbookmarkstatus action

--- app/components/BookmarkButton.jsx
+++ app/components/BookmarkButton.jsx
@@ -1,5 +1,7 @@
 'use client';
+import { useState, useEffect } from "react";
 import bookmarkProperty from "../actions/bookmarkProperty";
+import checkBookmarkStatus from "../actions/checkBookmarkStatus";
 import { toast } from "react-toastify";
 import { FaBookmark } from "react-icons/fa";
 import { useSession } from "next-auth/react";
@@ -7,7 +9,21 @@ import { useSession } from "next-auth/react";
 const BookmarkButton = ({property}) => {
   const { data: session } = useSession();
   const userId = session?.user?.id;
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  const [loading, setLoading] = useState(true);
   
+  useEffect(() => {
+    if(!userId){
+      setLoading(false);
+      return;
+    }
+    checkBookmarkStatus(property._id).then((res) => {
+      if(res.error) toast.error(res.error);
+      if(res.isBookmarked) setIsBookmarked(res.isBookmarked);
+      setLoading(false);
+    })
+  }, [property._id, userId, checkBookmarkStatus]);
+
   const handleClick = async () => {
     if(!userId){
       toast.error('User must log in to bookmark a listing');
@@ -17,17 +33,23 @@ const BookmarkButton = ({property}) => {
       if(res.error){
         return toast.error(res.error)
       }
+      setIsBookmarked(res.isBookmarked)
       toast.success(res.message);
     });
   }
 
-  return ( 
+  return isBookmarked ? ( 
     <button
+    className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+    onClick={handleClick}>
+      <FaBookmark className="mr-2"/> Remove Bookmark
+    </button>
+  ) : (    <button
     className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
     onClick={handleClick}>
       <FaBookmark className="mr-2"/> Bookmark Property
     </button>
-  );
+    );
 }
 
 export default BookmarkButton;
