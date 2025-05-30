62ff717141120fcf903d45f52e7ea3ca35a91beb
Codiemaureen <codiestover.cs@gmail.com>
2025-05-30 13:53:20 -0400

    Displaying property cards on search results

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -1,7 +1,10 @@
 import PropertyCard from "@/app/components/PropertyCard";
+import PropertySearchForm from "@/app/components/PropertySearchForm";
+import Link from "next/link";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 import { convertToSerializableObject } from "@/utils/convertToObject";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
 
 const SearchResults = async ({ searchParams }) => {
  let { location, propertyType } = await searchParams;
@@ -29,11 +32,28 @@ const SearchResults = async ({ searchParams }) => {
  const properties = convertToSerializableObject(propertiesQueryResults);
  
  
- return ( 
+ return  ( 
  <>
-  {properties.map((property) => (
-   <PropertyCard key={property._id} property={property} />
-  ))}
+  <section className="bg-blue-700 py-4">
+   <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+    <PropertySearchForm />
+   </div>
+  </section>
+  <section className="px-4 py-6">
+   <div className="contaner-xl lg:container m-auto px-4 py-6">
+    <Link href='/properties'className="flex items-center text-blue-500 hover:underline mb-3">
+    <FaArrowAltCircleLeft className="mr-2 mb-1"/>
+    </Link>
+    <h1 className="text-2xl mb-4">Search Results</h1>
+    {properties.length === 0 ? (<p>No Search Results</p>) : (
+    <div className="grid grid-cols-1 md:grid-cols-3">
+      {properties.map((property) => (
+       <PropertyCard key={property._id} property={property} />
+      ))}
+    </div>
+    )}
+   </div>
+  </section>
  </> );
 }
  
