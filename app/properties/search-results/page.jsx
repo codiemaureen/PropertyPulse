ace70a3256c801df447ac7b65fb35832a62b24c0
Codiemaureen <codiestover.cs@gmail.com>
2025-05-30 13:32:54 -0400

    adding properties to search results page

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -1,7 +1,40 @@
-const SearchResults = () => {
- return ( <div>
-  Search results
- </div> );
+import PropertyCard from "@/app/components/PropertyCard";
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { convertToSerializableObject } from "@/utils/convertToObject";
+
+const SearchResults = async ({ searchParams }) => {
+ let { location, propertyType } = await searchParams;
+ await connectDB();
+
+ const locationPattern = new RegExp(location, 'i');
+
+ let query = {
+  $or: [
+   {name: locationPattern},
+   {description: locationPattern},
+   {'location.street': locationPattern},
+   {'location.city': locationPattern},
+   {'location.state': locationPattern},
+   {'location.zipcode': locationPattern}
+  ]
+ }
+
+ if(propertyType && propertyType != 'All'){
+  const typePattern = new RegExp(propertyType, 'i');
+  query.type = typePattern;
+ }
+
+ const propertiesQueryResults = await Property.find(query).lean();
+ const properties = convertToSerializableObject(propertiesQueryResults);
+ 
+ 
+ return ( 
+ <>
+  {properties.map((property) => (
+   <PropertyCard key={property._id} property={property} />
+  ))}
+ </> );
 }
  
 export default SearchResults;
