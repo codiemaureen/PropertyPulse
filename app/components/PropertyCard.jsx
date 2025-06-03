6a1f99dd6f7eb88faaaf0f68de65d93ab8fc59dd
Codiemaureen <codiestover.cs@gmail.com>
2025-06-03 18:27:38 -0400

    making property card image a link

--- app/components/PropertyCard.jsx
+++ app/components/PropertyCard.jsx
@@ -15,6 +15,10 @@ const PropertyCard = ({property}) => {
  }
  return ( 
     <div className="rounded-xl shadow-md relative">
+      <Link
+        href={`/properties/${property._id}`}
+        className="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+      >   
       <Image
         src={property.images[0]}
         alt=""
@@ -23,6 +27,7 @@ const PropertyCard = ({property}) => {
         sizes="100vww"
         className="w-full h-auto rounded-t-xl"
       />
+      </Link>   
       <div className="p-4">
         <div className="text-left md:text-center lg:text-left mb-6">
           <div className="text-gray-600">{property.type}</div>
