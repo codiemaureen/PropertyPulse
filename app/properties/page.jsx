6a1a256975c92687bc3c26b1dd505728c0b99a91
Codiemaureen <codiestover.cs@gmail.com>
2025-06-03 19:00:03 -0400

    Implementing pagination

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -2,23 +2,29 @@ import PropertyCard from '../components/PropertyCard';
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
 
-const PropertiesPage = async () => {
+const PropertiesPage = async (props) => {
+  const searchParams = await props.searchParams || {};
+  const page = parseInt(searchParams.page || '1', 10);
+  const pageSize = parseInt(searchParams.pageSize || '2', 10);
+
   await connectDB();
-  const properties = await Property.find({}).lean();
+  const skip = (page - 1)* pageSize;
+  const total = await Property.countDocuments({}); 
+  const properties = await Property.find({}).skip(skip).limit(pageSize);
 
   return ( 
-  <section className='px-4 py-6'>
-   <div className='container-xl lg:container m-auto px-4 py-6'>
-    { properties.length === 0 ? (<p>no properties found</p>) : (
-     <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-      {properties.map((property) => (
-       <div key={property._id}><PropertyCard property={property}/></div>
-      ))}
-     </div>
-    )}
-   </div>
-  </section>
- );
+    <section className='px-4 py-6'>
+      <div className='container-xl lg:container m-auto px-4 py-6'>
+        { properties.length === 0 ? (<p>no properties found</p>) : (
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+          {properties.map((property) => (
+          <div key={property._id}><PropertyCard property={property}/></div>
+          ))}
+        </div>
+        )}
+      </div>
+    </section>
+  );
 }
- 
+
 export default PropertiesPage;
