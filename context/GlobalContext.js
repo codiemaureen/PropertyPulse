419ed0398a3ef0990bbf4a67ccf8fcf5b87d07ba
Codiemaureen <codiestover.cs@gmail.com>
2025-06-03 17:34:22 -0400

    fixing varialbe typo

--- context/GlobalContext.js
+++ context/GlobalContext.js
@@ -1,21 +1,35 @@
 'use client';
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState , useEffect} from "react";
+import { useSession } from "next-auth/react";
+import getUnreadMessageCount from "@/app/actions/getUnreadMessageCount";
 
 const GlobalContext = createContext();
 
 export function GlobalProvider({children}) {
- const [unreadCount, setunreadCount] = useState(0);
+  const [unreadCount, setUnreadCount] = useState(0);
 
- return (
- <GlobalContext.Provider value={{
-  unreadCount,
-  setunreadCount
-  }}>
-   {children}
-  </GlobalContext.Provider>)
+  const { data: session} = useSession();
+
+
+  useEffect(() => {
+    if(session && session.user){
+      getUnreadMessageCount().then((res) => {
+        if(res.count) setunreadCount(res.count);
+      })
+    }
+  }, [getUnreadMessageCount, session])
+
+
+  return (
+  <GlobalContext.Provider value={{
+    unreadCount,
+    setUnreadCount
+    }}>
+    {children}
+    </GlobalContext.Provider>)
 }
 
 export function useGlobalContext(){
- return useContext(GlobalContext);
+  return useContext(GlobalContext);
 }
 
