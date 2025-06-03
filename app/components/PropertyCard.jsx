40225b868537aaee3d2e9acd13850c6b4de45505
Codiemaureen <codiestover.cs@gmail.com>
2025-06-03 18:50:52 -0400

    removing class to fix bug

--- app/components/PropertyCard.jsx
+++ app/components/PropertyCard.jsx
@@ -17,7 +17,6 @@ const PropertyCard = ({property}) => {
     <div className="rounded-xl shadow-md relative">
       <Link
         href={`/properties/${property._id}`}
-        className="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm"
       >   
       <Image
         src={property.images[0]}
